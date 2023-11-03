9dd8f81f048481dcbfb051da929b204f187813b1
Paloma Peralta <palomaperalta9@gmail.com>
2023-11-02 21:05:53 -0300

    lista de tareas

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [valorInput, setValorInput] = useState("");
+  const [listaDeTareas, setListaDeTareas] = useState([]);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    let tarea = {
+      textoDeTarea: valorInput,
+      id: listaDeTareas.length + 1,
+    };
+    setListaDeTareas([...listaDeTareas, tarea]);
+    setValorInput("");
+  };
+
+  const handleOnClick = (idTarea) => {
+    const listaDeTareasFiltrada = listaDeTareas.filter(
+      (tarea) => tarea.id !== idTarea
+    );
+    setListaDeTareas(listaDeTareasFiltrada);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center min-h-screen w-full">
+      <div className=" flex justify-center w-3/12 gap-8 flex-col">
+        <form
+          className="border-solid rounded-sm border-4 border-fuchsia-500 flex justify-between gap-8 p-4 shadow-[-15px_15px_0_0] shadow-fuchsia-500"
+          onSubmit={(e) => handleOnSubmit(e)}
+        >
+          <input
+            type="text"
+            placeholder="What is your task?"
+            value={valorInput}
+            onChange={(event) => {
+              setValorInput(event.target.value);
+            }}
+          />
+
+          <button type="submit">+</button>
+        </form>
+        <div className="flex gap-8 flex-col justify-center">
+          {listaDeTareas.map((tarea) => {
+            return (
+              <div
+                className="border-solid
+                  rounded-sm
+                  border-4
+                  border-fuchsia-500 p-4 shadow-[-15px_15px_0_0] shadow-fuchsia-500
+                  flex justify-between"
+                key={tarea.id}
+              >
+                {tarea.textoDeTarea}
+                <button onClick={() => handleOnClick(tarea.id)}>-</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
